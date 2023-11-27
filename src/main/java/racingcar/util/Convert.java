7c6632a0ff3979a734fedabf9528dba9e7153aa5
YongHyeon <pyh4616@naver.com>
2023-11-27 18:42:46 +0900

    feat(Convert): 문자열을 정수로 변환하는 기능 구현

--- src/main/java/racingcar/util/Convert.java
+++ src/main/java/racingcar/util/Convert.java
@@ -6,8 +6,17 @@ import java.util.List;
 public class Convert {
 
     private static final String COMMA = ",";
+    private static final String NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 숫자만 입력할 수 있습니다.";
 
     public static List<String> StringToListByComma(String input) {
         return Arrays.asList(input.split(COMMA));
+    }
+
+    public static int StringToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_FORMAT_ERROR_MESSAGE);
+        }
     }
 }
