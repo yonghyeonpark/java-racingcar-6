d4b247c5cbe3ba62ecb95047a9e1b2f1fcd2e47a
YongHyeon <pyh4616@naver.com>
2023-11-27 17:15:09 +0900

    feat(Cars): 경주 할 자동차 이름 입력에 대한 유효성 검사 기능 구현

---
+++ src/main/java/racingcar/model/Cars.java
@@ -0,0 +1,48 @@
+package racingcar.model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import racingcar.util.Convert;
+
+public class Cars {
+
+    private static final String CARS_SIZE_ERROR_MESSAGE = "[ERROR] 참가 자동차의 수의 범위는 1~8입니다.";
+    private static final String CAR_NAME_RANGE_ERROR_MESSAGE = "[ERROR] 자동차 이름의 범위는 1~5자입니다.";
+    private static final String CAR_NAMES_DUPLICATE_MESSAGE = "[ERROR] 자동차의 이름은 중복될 수 없습니다.";
+
+    private final List<String> cars;
+
+    public Cars(String inputCars) {
+        this.cars = convertAndValidate(inputCars);
+    }
+
+    private List<String> convertAndValidate(String inputCars) {
+        List<String> cars = Convert.StringToListByComma(inputCars);
+        validateSize(cars);
+        validateNameLength(cars);
+        validateDuplicate(cars);
+        return cars;
+    }
+
+    private void validateSize(List<String> cars) {
+        if (cars.size() > 8 || cars.size() < 1) {
+            throw new IllegalArgumentException(CARS_SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    private void validateNameLength(List<String> cars) {
+        for (String car : cars) {
+            if (car.length() < 1 || car.length() > 5) {
+                throw new IllegalArgumentException(CAR_NAME_RANGE_ERROR_MESSAGE);
+            }
+        }
+    }
+
+    private void validateDuplicate(List<String> cars) {
+        Set<String> duplicateCheck = new HashSet<>(cars);
+        if (duplicateCheck.size() != cars.size()) {
+            throw new IllegalArgumentException(CAR_NAMES_DUPLICATE_MESSAGE);
+        }
+    }
+}
