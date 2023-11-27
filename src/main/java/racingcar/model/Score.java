31a4c9d42d3c593dd35ed544145cd32d5aec7fdf
YongHyeon <pyh4616@naver.com>
2023-11-27 18:14:16 +0900

    feat(Score): 자동차 경주 진행 상태를 나타내는 toString 구현

--- src/main/java/racingcar/model/Score.java
+++ src/main/java/racingcar/model/Score.java
@@ -2,11 +2,14 @@ package racingcar.model;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Score {
 
     private static final int INITIAL_VALUE = 0;
     private static final int INCREMENTAL_VALUE = 1;
+    private static final String CAR_MOVING_RESULT_FORMAT = " : ";
+    private static final String MOVING = "-";
 
     private final LinkedHashMap<String, Integer> score;
 
@@ -23,5 +26,18 @@ public class Score {
 
     public void addMoving(String carName) {
         score.put(carName, score.get(carName) + INCREMENTAL_VALUE);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : score.entrySet()) {
+            stringBuilder.append(entry.getKey())
+                    .append(CAR_MOVING_RESULT_FORMAT);
+            for (int i = 0; i < entry.getValue(); i++) {
+                stringBuilder.append(MOVING);
+            }
+        }
+        return stringBuilder.toString();
     }
 }
