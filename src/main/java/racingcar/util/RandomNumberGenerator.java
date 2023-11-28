384cd19a72518d40839abc1f97170b7e914df122
YongHyeon <pyh4616@naver.com>
2023-11-28 21:14:11 +0900

    refactor(Cars, RandomNumberGenerator): 매직 넘버를 상수로 선언

--- src/main/java/racingcar/util/RandomNumberGenerator.java
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -4,7 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberGenerator {
 
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 9;
+
     public int execute() {
-        return Randoms.pickNumberInRange(0, 9);
+        return Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
     }
 }
